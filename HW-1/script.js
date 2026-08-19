a464271c1fcbcff9343becf8936caf94070f9dde
Mark Pavliv <mark.pavliv15@gmail.com>
2026-08-19 13:26:22 +0200

    added new hw1

---
+++ HW-1/script.js
@@ -0,0 +1,36 @@
+const name = "mark";
+
+console.log(name);
+
+let age = 14;
+
+console.log(age);
+
+let isStudent = false;
+
+console.log(isStudent);
+
+const mystring = "любовь не спички скибиди яички";
+
+console.log(mystring);
+
+let mynumber = 67;
+mynumber = mynumber + 10;
+
+console.log(mynumber);
+
+let myNull = null;
+
+console.log(myNull);
+
+const userName = prompt("Введіть своє ім’я");
+
+alert(userName);
+
+console.log(userName);
+
+confirm("Ви дійсно хочете покинути сторінку?");
+
+alert("Увага! Дія може бути небезпечною");
+
+confirm("Ви впевнені, що хочете продовжити?");
