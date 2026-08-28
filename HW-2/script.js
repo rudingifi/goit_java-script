40e1976ef7f9accbc69f91255dd0d44887e00eef
Mark Pavliv <mark.pavliv15@gmail.com>
2026-08-28 15:04:42 +0200

    HW-2

---
+++ HW-2/script.js
@@ -0,0 +1,76 @@
+const celsius = 20;
+
+console.log(celsius);
+
+let fahrenheit = (celsius * 1.8) + 32;
+
+console.log(fahrenheit);
+
+const daysInMonth = 30;
+
+console.log(daysInMonth);
+
+const hoursInMonth = (daysInMonth * 24);
+
+console.log(hoursInMonth);
+
+const minutesInMonth = (hoursInMonth * 60);
+
+console.log(minutesInMonth);
+
+let health = 100;
+health -= 50;
+
+console.log(health);
+
+let totalPrice = 100;
+
+console.log(totalPrice);
+
+const discount = 10;
+
+console.log(discount);
+
+let discountedPrice = (totalPrice - discount);
+
+console.log(discountedPrice);
+
+const floatNumber = 12.78;
+
+console.log(Math.floor(floatNumber));
+
+const roundedDown = 12;
+
+console.log(roundedDown)
+
+const floatString = "45.67";
+
+console.log(Number.parseFloat(floatString));
+
+const parsedFloat = 45.67 ;
+
+console.log(parsedFloat);
+
+const intString = "123";
+
+console.log(Number.parseInt(intString));
+
+const parsedInt = 123;
+
+console.log(parsedInt);
+
+const number = 6 ;
+
+console.log(Math.pow(number,2));
+
+const sqrtNumber = 36;
+
+console.log(sqrtNumber);
+
+const integer = 42;
+
+console.log(integer.toString());
+
+const stringNumber = "256";
+
+console.log(Number.parseInt(stringNumber));
