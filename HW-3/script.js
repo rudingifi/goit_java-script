8d5ebab93b238680ddb7fb4d7f370b05703874ab
Mark Pavliv <mark.pavliv15@gmail.com>
2026-09-06 14:02:55 +0200

    HW-3

---
+++ HW-3/script.js
@@ -0,0 +1,19 @@
+const result = 5+5+`5`;
+
+console.log(typeof result);
+
+const email = prompt(`write ur email`);
+
+console.log(email.includes(`@`));
+console.log(email.length);
+
+const word1 = `my`;
+const word2 = `name`;
+const word3 = `is`;
+const fullName = (`${word1} ${word2} ${word3}`) ;
+
+console.log(`${fullName} Viktor`);
+
+const userName = prompt(`write ur name`);
+
+const payment = alert(`Дякуємо, ${userName}! До сплати 300 гривень`);
