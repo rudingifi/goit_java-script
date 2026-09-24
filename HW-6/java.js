6a4e7132eab985ee666da7a14e8d470265f9ce52
Mark Pavliv <mark.pavliv15@gmail.com>
2026-09-24 16:56:09 +0200

    HW-6

---
+++ HW-6/java.js
@@ -0,0 +1,39 @@
+let number = 1;
+
+while (number <= 10) {
+    console.log(number);
+    number+=1
+}
+
+
+
+for(let i = 0; i <= 20; i += 1){
+
+    if(i % 2 === 0){
+        console.log(i);
+    }
+    
+}
+
+
+for (let i = 1; i <= 10; i++) {
+  console.log(7 * i);
+}
+
+
+let n = 100;
+
+for (let i = 0; i <= 100; i += 1){
+    console.log(i);
+    if(i >= n){
+        break
+    }
+}
+
+for(let i = 0; i <= 20; i += 1){
+
+    if(i % 3 === 0){
+        console.log(i);
+    }
+    
+}
